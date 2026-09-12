df20a5e57fa49433c32dbe9a882abc7e0dd31c12
Zaza Mchedlidze <mchedlidze.zaza1000@gmail.com>
2026-09-12 17:31:59 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4228,26 +4228,7 @@ async function saveMeeting( event ) {
     }
     toast( 'კლუბის შეკრება გამოქვეყნდა.', 'success' )
     
-try {
-      const res = await fetch("https://corsproxy.io/?url=" + encodeURIComponent("https://onesignal.com/api/v1/notifications"), {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "Authorization": "Basic os_v2_app_d4psobll6rhhpg3ec66v3ae6yqagkpebweheqwnabdw4hmhlrijhncsfbiboyqyzefj5p2itv4zaujrlxccbpsaan7cegyawzjesvva"
-        },
-        body: JSON.stringify({
-          app_id: "1f1f2705-6bf4-4e77-9b64-17bd5d809ec4",
-          included_segments: ["All"],
-          headings: { en: "ArduinoHub - ახალი შეხვედრა 🔔" },
-          contents: { en: `ჩაინიშნა კლუბის შეკრება! თარიღი: ${date}, დრო: ${time}` }
-        })
-      });
-      const resData = await res.json();
-      console.log("OneSignal Response:", resData);
-    } catch (err) {
-      console.error("Error sending push notification:", err);
-    }
-    
+
     await loadAdminMeeting()
   } catch (error) {
     errorTarget.textContent = neutralError( error, 'შეკრების გამოქვეყნება ვერ მოხერხდა.' )
